012bf75418f221d6e866732c42cdf0895c4010d2
Babak Nikanjam <babak.nikanjam@gmail.com>
2019-11-23 20:14:18 -0800

    /users/<user_id> GET READ 200 'Retrieve a single user'

--- project/tests/functional/test_users.py
+++ project/tests/functional/test_users.py
@@ -1,5 +1,8 @@
 import json
 
+from project.api.models import User
+from project import db
+
 
 def test_add_user(test_app, test_database):
     client = test_app.test_client()
@@ -64,4 +67,37 @@ def test_add_user_duplicate_email(test_app, test_database):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 400
     assert 'Sorry. That email already exists.' in data['message']
+    assert 'fail' in data['status']
+
+
+def test_single_user(test_app, test_database):
+    user = User(username='steve', email='steve@apple.com')
+    db.session.add(user)
+    db.session.commit()
+
+    client = test_app.test_client()
+    resp = client.get(f'/users/{user.id}')
+    data = json.loads(resp.data.decode())
+
+    assert resp.status_code == 200
+    assert 'steve' in data['data']['username']
+    assert 'steve@apple.com' in data['data']['email']
+    assert 'success' in data['status']
+
+
+def test_single_user_no_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get('/users/blah')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'User does not exist' in data['message']
+    assert 'fail' in data['status']
+
+
+def test_single_user_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get('/users/999')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'User does not exist' in data['message']
     assert 'fail' in data['status']
