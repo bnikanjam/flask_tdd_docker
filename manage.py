c717944a6b0e4d93c67ee2f6b3fc4c2fa6c18337
Babak Nikanjam <babak.nikanjam@gmail.com>
2019-12-20 16:15:31 -0800

    Test: /users/:id  PUT  UPDATE  update a user

--- manage.py
+++ manage.py
@@ -10,23 +10,23 @@ app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
 
-@cli.command('recreate_db')
+@cli.command("recreate_db")
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
 
 
-@cli.command('seed_db')
+@cli.command("seed_db")
 def seed_db():
     """Seeds db with some initial data."""
-    db.session.add(User(username='babak', email="user1@gmail.com"))
-    db.session.add(User(username='user2', email="user2@gmail.com"))
-    db.session.add(User(username='user3', email="user3@gmail.com"))
-    db.session.add(User(username='user4', email="user4@gmail.com"))
-    db.session.add(User(username='user5', email="user5@gmail.com"))
+    db.session.add(User(username="babak", email="user1@gmail.com"))
+    db.session.add(User(username="user2", email="user2@gmail.com"))
+    db.session.add(User(username="user3", email="user3@gmail.com"))
+    db.session.add(User(username="user4", email="user4@gmail.com"))
+    db.session.add(User(username="user5", email="user5@gmail.com"))
     db.session.commit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
